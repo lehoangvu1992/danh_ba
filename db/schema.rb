3eb0d12d2b76a33f10e58ded54c9309f0f24d787
lehoangvu <thuonghoai.financial@gmail.com>
2016-03-21 14:15:18 +0700

    fix avatar :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320214629) do
+ActiveRecord::Schema.define(version: 20160321064632) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name",               limit: 255
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20160320214629) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
