964c89e3215f1061a6879f40a203633883dd69fd
lehoangvu <thuonghoai.financial@gmail.com>
2016-03-21 11:03:38 +0700

    fix code

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
 	def latest
  	  if user_signed_in? 
  	  	redirect_to my_contacts_path
+ 	  else
+ 	  	redirect_to new_user_session_path
  	  end	
 	end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,13 @@
 class Contact < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 20 }
+  validates :number, presence: true, length: { maximum: 11 }
+  validates :address, presence: true, length: {maximum: 20}
+  validates :note, presence: true, length: {maximum: 50}
 	belongs_to :user
+
 	
 	has_attached_file :image , styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	
 	def self.to_csv(options = {})
 		CSV.generate(options) do |csv|
