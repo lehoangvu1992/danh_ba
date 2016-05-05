c2129c5419b1e1f97b5de66007e7855777b1f977
Le Hoang Vu <lehoangvu1992@gmail.com>
2016-05-05 20:58:09 +0700

    test user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
 	    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :avatar) }
 	    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :avatar) }
 	  end
+	  
 end
